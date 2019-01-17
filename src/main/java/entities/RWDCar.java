9de59bf313311286e5e3e91a23e1e050c5093730
Stefan Hagiu <sxh1076@cs.bham.ac.uk>
2019-01-17 21:40:12 +0000

    Added RWDCar and its constructor

--- src/main/java/entities/RWDCar.java
+++ src/main/java/entities/RWDCar.java
@@ -1,4 +1,19 @@
-package entities.abstractCarComponents;
+package entities;
 
-public class Car {
+import entities.abstractCarComponents.*;
+
+public class RWDCar extends PhysicalEntity {
+    public RWDCar(CarBody carBody, Differential backDifferential, DriveShaft driveShaft, Engine engine,
+                  Gearbox gearbox, Springs springs, Wheel flWheel, Wheel frWheel, Wheel blWheel, Wheel brWheel) {
+        this.addComponent(carBody);
+        this.addComponent(backDifferential);
+        this.addComponent(driveShaft);
+        this.addComponent(engine);
+        this.addComponent(gearbox);
+        this.addComponent(springs);
+        this.addComponent(flWheel);
+        this.addComponent(frWheel);
+        this.addComponent(blWheel);
+        this.addComponent(brWheel);
+    }
 }

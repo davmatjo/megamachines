4e47010740d7983dc0f09fcc454c135dbd816c0e
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-01-20 13:08:38 +0000

    Fix track generation, corner peices Gameobject should retain size

--- src/main/java/com/battlezone/megamachines/world/GameObject.java
+++ src/main/java/com/battlezone/megamachines/world/GameObject.java
@@ -5,12 +5,27 @@ import org.joml.Vector2f;
 public class GameObject {
 
     private Vector2f position;
+    private float scale;
 
-    public GameObject(Vector2f position) {
+    public GameObject(Vector2f position, float scale) {
+        this.position = position;
+        this.scale = scale;
+    }
+
+    public void setPosition(Vector2f position) {
         this.position = position;
     }
 
     public Vector2f getPosition() {
         return position;
     }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
 }
--- src/main/java/com/battlezone/megamachines/world/Track.java
+++ src/main/java/com/battlezone/megamachines/world/Track.java
@@ -12,8 +12,8 @@ public class Track extends GameObject {
 
     private TrackType type;
 
-    private Track(Vector2f position, TrackType type) {
-        super(position);
+    private Track(Vector2f position, float scale, TrackType type) {
+        super(position, scale);
         this.type = type;
     }
 
@@ -71,7 +71,7 @@ public class Track extends GameObject {
             for (int j = 0; j < tracksDown; j++) {
                 TrackType type = world[i][j];
                 if (type != null) {
-                    Track t = new Track(new Vector2f((tracksAcross - i - 1) * trackSize, j * trackSize), world[i][j]);
+                    Track t = new Track(new Vector2f((tracksAcross - i - 1) * trackSize, (tracksDown - j - 1) * trackSize), trackSize, world[i][j]);
                     track.add(t);
                 }
             }

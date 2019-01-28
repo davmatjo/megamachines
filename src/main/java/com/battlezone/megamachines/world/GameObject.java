4889752316284f292972f2bf4839db3e96bc6cd8
Stefan Hagiu <sxh1076@cs.bham.ac.uk>
2019-01-28 11:13:05 +0000

    Added comments

--- src/main/java/com/battlezone/megamachines/world/GameObject.java
+++ src/main/java/com/battlezone/megamachines/world/GameObject.java
@@ -3,11 +3,23 @@ package com.battlezone.megamachines.world;
 import com.battlezone.megamachines.renderer.game.AbstractRenderable;
 import com.battlezone.megamachines.renderer.game.Model;
 
-
+/**
+ * A game object.
+ * Everything that moves or collides is a game object.
+ */
 public abstract class GameObject extends AbstractRenderable {
 
+    /**
+     * The x coordinate of this game object
+     */
     private double x;
+    /**
+     * The y coordinate of this game object
+     */
     private double y;
+    /**
+     * The scale of this game object
+     */
     private float scale;
 
     /**
@@ -15,6 +27,13 @@ public abstract class GameObject extends AbstractRenderable {
      */
     private double speed;
 
+    /**
+     * The constructor
+     * @param x The x coordinate
+     * @param y The y coordinate
+     * @param scale The scale
+     * @param model The model
+     */
     public GameObject(double x, double y, float scale, Model model) {
         super(model);
         this.x = x;
@@ -22,34 +41,67 @@ public abstract class GameObject extends AbstractRenderable {
         this.scale = scale;
     }
 
+    /**
+     * Sets the x coordinate
+     * @param x The x coordinate
+     */
     public void setX(double x) {
         this.x = x;
     }
 
-    public void setY(double y) {
-        this.y = y;
-    }
-
+    /**
+     * Gets the x coordinate
+     * @return The x coordinate
+     */
     public double getX() {
         return x;
     }
 
-    public double getY() {
-        return y;
-    }
-
+    /**
+     * Gets the x coordinate as a float
+     * @return The x coordinate as a float
+     */
     public float getXf() {
         return (float) getX();
     }
 
+
+    /**
+     * Sets the y coordinate
+     * @param y The y coordinate
+     */
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    /**
+     * Gets the y coordinate
+     * @return The y coordinate
+     */
+    public double getY() {
+        return y;
+    }
+
+    /***
+     * Gets the y coordinate as a float
+     * @return The y coordinate as a float
+     */
     public float getYf() {
         return (float) getY();
     }
 
+    /**
+     * Gets the scale of this game object
+     * @return The scale of this game object
+     */
     public float getScale() {
         return scale;
     }
 
+    /**
+     * Sets the scale of this game object
+     * @param scale The sclae of this game object
+     */
     public void setScale(float scale) {
         this.scale = scale;
     }

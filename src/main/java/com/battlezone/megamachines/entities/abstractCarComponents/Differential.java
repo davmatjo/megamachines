8d47e54bdd63d1d0024f0343a24b2b55c42ea43a
Stefan Hagiu <sxh1076@cs.bham.ac.uk>
2019-02-02 23:05:16 +0000

    Refactored RearDifferential and Differential

--- src/main/java/com/battlezone/megamachines/entities/abstractCarComponents/Differential.java
+++ src/main/java/com/battlezone/megamachines/entities/abstractCarComponents/Differential.java
@@ -7,13 +7,43 @@ import com.battlezone.megamachines.entities.EntityComponent;
  */
 public abstract class Differential extends EntityComponent {
     /**
+     * The wheel on the rear left of the car
+     */
+    protected Wheel leftWheel;
+
+    /**
+     * The wheel on the rear right of the car
+     */
+    protected Wheel rightWheel;
+
+    /**
+     * The final drive ratio of this differential
+     */
+    protected double finalDriveRatio;
+
+    /**
      * Sends torque to the wheels
      * @param torque The torque to be sent
      */
-    public abstract void sendTorque(double torque);
+    public void sendTorque(double torque) {
+        torque *= finalDriveRatio;
+
+        double inertiaPerWheel = (leftWheel.getWeight() * (leftWheel.getDiameter() / 2) * (leftWheel.getDiameter() / 2)) / 2;
+        double wheelInertia = 2 * inertiaPerWheel;
+
+        double angularAcceleration = torque / wheelInertia;
+
+        leftWheel.applyAcceleration(angularAcceleration);
+        rightWheel.applyAcceleration(angularAcceleration);
+    }
 
     /**
      * Gets the differential's new RPM (at the drive shaft end)
      */
-    public abstract double getNewRPM();
+    public double getNewRPM() {
+        double lowerSpeed = Math.min(leftWheel.getAngularVelocity(), rightWheel.getAngularVelocity());
+
+        double newRPM = (60 * lowerSpeed / Math.PI) * finalDriveRatio;
+        return newRPM;
+    }
 }

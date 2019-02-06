f127da535a3401347930d14a18e050d8a7dcba21
Sebastian Stanici <sxs1476@student.bham.ac.uk>
2019-02-06 15:20:24 +0000

    Created Protocol class

--- src/main/java/com/battlezone/megamachines/networking/NewServer.java
+++ src/main/java/com/battlezone/megamachines/networking/NewServer.java
@@ -12,12 +12,11 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.battlezone.megamachines.networking.Protocol.KEY_EVENT;
+import static com.battlezone.megamachines.networking.Protocol.KEY_PRESSED;
 import static java.util.Arrays.copyOfRange;
 
 public class NewServer {
-
-    // Define the states of the server
-    private enum State{LOBBY, IN_GAME}
 
     public NewServer() throws SocketException {
         this.socket = new DatagramSocket();
@@ -25,18 +24,7 @@ public class NewServer {
         this.send = new DatagramPacket(new byte[258], 258);
     }
 
-    // Define constant server event types for Client to Server packets -> on byte 0
-    private static final byte JOIN_LOBBY = 0;
-    private static final byte START_GAME = 1;
-    private static final byte KEY_EVENT = 2;
 
-    // Define constant key event types for Client to Server packets -> on byte 1
-    private static final byte KEY_PRESSED = 0;
-    private static final byte KEY_RELEASED = 1;
-
-    // Define constant for types of packets for Server to Client -> on byte 0
-    private static final byte GAME_STATE = 0;
-    private static final byte TRACK_TYPE = 1;
 
     // Define other constants
     private static final int MAX_PLAYERS = 8;
@@ -47,7 +35,7 @@ public class NewServer {
     static final int SERVER_TO_CLIENT_LENGTH = 300;
 
     private boolean running = true;
-    private State currentState = State.LOBBY;
+    private Protocol.State currentState = Protocol.State.LOBBY;
     private final DatagramSocket socket;
     private final DatagramPacket receive;
     private final DatagramPacket send;
@@ -61,15 +49,15 @@ public class NewServer {
             try {
                 socket.receive(receive);
                 byte[] received = receive.getData();
-                if (received[0] == JOIN_LOBBY) {
+                if (received[0] == Protocol.JOIN_LOBBY) {
                     // Make the first player as the host
                     if ( players.isEmpty() )
                         host = receive.getAddress();
                     // Add new players to the player ArrayList if there's less than MAX_PLAYERS amount
                     if ( players.size() < MAX_PLAYERS )
                         players.put(receive.getAddress(), new Player((int) received[1], Vector3f.fromByteArray(received, 2)));
-                } if ( received[0] == START_GAME && receive.getAddress().equals(host) ) {
-                    currentState = State.IN_GAME;
+                } if ( received[0] == Protocol.START_GAME && receive.getAddress().equals(host) ) {
+                    currentState = Protocol.State.IN_GAME;
                     initGame(players);
                 }
             } catch (IOException e) {
@@ -128,7 +116,7 @@ public class NewServer {
     public void sendGameState(Map<InetAddress, Player> players) {
         // Set data to game state
         ByteBuffer byteBuffer = ByteBuffer.allocate(players.size()*32+2);
-        byteBuffer.put(GAME_STATE);
+        byteBuffer.put(Protocol.GAME_STATE);
         byteBuffer.put((byte) players.size());
         for ( int i = 0; i < players.size(); i++ ) {
             byteBuffer.putDouble(players.get(i).getCar().getX());
---
+++ src/main/java/com/battlezone/megamachines/networking/Protocol.java
@@ -0,0 +1,19 @@
+package com.battlezone.megamachines.networking;
+
+public class Protocol {
+    // Define the states of the server
+    enum State{LOBBY, IN_GAME}
+
+    // Define constant server event types for Client to Server packets -> on byte 0
+    static final byte JOIN_LOBBY = 0;
+    static final byte START_GAME = 1;
+    static final byte KEY_EVENT = 2;
+
+    // Define constant key event types for Client to Server packets -> on byte 1
+    static final byte KEY_PRESSED = 0;
+    static final byte KEY_RELEASED = 1;
+
+    // Define constant for types of packets for Server to Client -> on byte 0
+    static final byte GAME_STATE = 0;
+    static final byte TRACK_TYPE = 1;
+}

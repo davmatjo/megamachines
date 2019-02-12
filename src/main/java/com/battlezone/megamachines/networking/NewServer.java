47c094c266e9017d04fad9dcf14f495784bf36ef
Sebastian Stanici <sxs1476@student.bham.ac.uk>
2019-02-12 22:09:03 +0000

    Added a TODO for issue

--- src/main/java/com/battlezone/megamachines/networking/NewServer.java
+++ src/main/java/com/battlezone/megamachines/networking/NewServer.java
@@ -15,7 +15,7 @@ import java.util.*;
 public class NewServer {
 
     // Constants
-    public static final int MAX_PLAYERS = 7;
+    public static final int MAX_PLAYERS = 7; // TODO: fix bug to make it work with 8 or more
     static final int PORT = 6970;
     public static final int SERVER_TO_CLIENT_LENGTH = 300;
     public static final int CLIENT_TO_SERVER_LENGTH = 14;

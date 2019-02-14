e3eeeb330046814412785716c4fccf8e6f299138
David Jones <dxj786@student.bham.ac.uk>
2019-02-14 01:05:52 +0000

    Further lobby improvements

--- src/main/java/com/battlezone/megamachines/world/Lobby.java
+++ src/main/java/com/battlezone/megamachines/world/Lobby.java
@@ -11,12 +11,14 @@ import com.battlezone.megamachines.messaging.EventListener;
 import com.battlezone.megamachines.messaging.MessageBus;
 import com.battlezone.megamachines.networking.Client;
 import com.battlezone.megamachines.networking.Protocol;
+import com.battlezone.megamachines.networking.Server;
 import com.battlezone.megamachines.renderer.Texture;
 import com.battlezone.megamachines.renderer.Window;
 import com.battlezone.megamachines.renderer.ui.Box;
 import com.battlezone.megamachines.renderer.ui.Button;
 import com.battlezone.megamachines.renderer.ui.Colour;
 import com.battlezone.megamachines.renderer.ui.Scene;
+import com.battlezone.megamachines.util.AssetManager;
 import com.battlezone.megamachines.world.track.Track;
 
 import java.io.IOException;
@@ -32,6 +34,21 @@ import static org.lwjgl.opengl.GL30.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL30.glClear;
 
 public class Lobby {
+
+    private static final float PLAYER_AVATAR_WIDTH = 0.4f;
+    private static final float PLAYER_AVATER_HEIGHT = 0.2f;
+    private static final float PLAYER_AVATAR_POSITION_OFFSET = 0.5f;
+    private static final float PLAYER_AVATAR_X = -1f;
+    private static final float PLAYER_AVATAR_Y_TOP = 0.5f;
+    private static final float PLAYER_AVATAR_Y_BOTTOM = 0f;
+    private static final float PADDING = 0.1f;
+
+    private static final float BUTTON_ROW_HEIGHT = 0.3f;
+    private static final float BUTTON_WIDTH = 1f;
+    private static final float BUTTON_ROW_Y = -0.5f;
+    private static final float CENTRAL_BUTTON_X = -BUTTON_WIDTH / 2;
+    private static final float RIGHT_BUTTON_X = PADDING / 2;
+    private static final float LEFT_BUTTON_X = -BUTTON_WIDTH - PADDING / 2;
 
     private final Queue<byte[]> playerUpdates = new ConcurrentLinkedQueue<>();
     private final Queue<byte[]> trackUpdates = new ConcurrentLinkedQueue<>();
@@ -60,6 +77,9 @@ public class Lobby {
         List<Box> models = new ArrayList<>();
         int port = 0;
 
+        Button quit = new Button(BUTTON_WIDTH, BUTTON_ROW_HEIGHT, CENTRAL_BUTTON_X, BUTTON_ROW_Y, Colour.WHITE, Colour.BLUE, "QUIT", PADDING, cursor);
+        lobby.addElement(quit);
+
         while (!glfwWindowShouldClose(gameWindow)) {
             glfwPollEvents();
 
@@ -68,16 +88,31 @@ public class Lobby {
                 players = RWDCar.fromByteArray(playerUpdates, 1);
                 if (!isHost && playerNumber == 0) {
                     isHost = true;
-                    Button start = new Button(
-                            1f, 0.5f, -0.5f, -0.5f, Colour.WHITE, Colour.BLUE, "START", 0.05f, cursor);
-                    lobby.addElement(start);
+
+                    Button start = new Button(BUTTON_WIDTH, BUTTON_ROW_HEIGHT, RIGHT_BUTTON_X, BUTTON_ROW_Y, Colour.WHITE, Colour.BLUE, "START", PADDING, cursor);
                     start.setAction(client::startGame);
+
+                    Button repositionedQuit = new Button(BUTTON_WIDTH, BUTTON_ROW_HEIGHT, LEFT_BUTTON_X, BUTTON_ROW_Y, Colour.WHITE, Colour.BLUE, "QUIT", PADDING, cursor);
+                    repositionedQuit.setAction(() -> System.out.println("quit"));
+                    lobby.removeElement(quit);
+                    quit.delete();
+                    quit = null;
+                    lobby.addElement(start);
+                    lobby.addElement(repositionedQuit);
+
                 }
                 models.forEach(lobby::removeElement);
                 models.forEach(Box::delete);
                 models.clear();
                 for (int i = 0; i < players.size(); i++) {
-                    models.add(new Box(0.2f, 0.4f, i * 0.5f, 0.5f, new Vector4f(players.get(i).getColour(), 1f), Texture.CIRCLE));
+                    models.add(
+                            new Box(
+                                    PLAYER_AVATAR_WIDTH,
+                                    PLAYER_AVATER_HEIGHT,
+                                    PLAYER_AVATAR_X + (i % (Server.MAX_PLAYERS / 2)) * PLAYER_AVATAR_POSITION_OFFSET,
+                                    i > Server.MAX_PLAYERS / 2 ? PLAYER_AVATAR_Y_BOTTOM : PLAYER_AVATAR_Y_TOP,
+                                    new Vector4f(players.get(i).getColour(), 1f),
+                                    AssetManager.loadTexture("/cars/car" + players.get(i).getModelNumber() + ".png")));
                 }
                 models.forEach(lobby::addElement);
             }

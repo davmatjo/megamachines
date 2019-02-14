1180e2fe96d761a7cb168e09b6d26cb477672338
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-02-14 13:04:30 +0000

    in game music

--- src/main/java/com/battlezone/megamachines/NewMain.java
+++ src/main/java/com/battlezone/megamachines/NewMain.java
@@ -2,6 +2,7 @@ package com.battlezone.megamachines;
 
 import com.battlezone.megamachines.entities.Cars.DordConcentrate;
 import com.battlezone.megamachines.entities.RWDCar;
+import com.battlezone.megamachines.events.game.GameStateEvent;
 import com.battlezone.megamachines.input.Cursor;
 import com.battlezone.megamachines.math.Vector3f;
 import com.battlezone.megamachines.messaging.MessageBus;
@@ -94,6 +95,7 @@ public class NewMain {
     }
 
     private void startSingleplayer() {
+        MessageBus.fire(new GameStateEvent(GameStateEvent.GameState.PLAYING));
         menu.hide();
         Track track = new TrackCircleLoop(20, 20, true).generateTrack();
         TrackPiece startPiece = track.getStartPiece();

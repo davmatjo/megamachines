d05b1b37992b916fdb332e359114f35cb3419de9
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-02-17 22:58:45 +0000

    Move saving vectors up to the abstract storage provider

--- src/main/java/com/battlezone/megamachines/storage/StorageProvider.java
+++ src/main/java/com/battlezone/megamachines/storage/StorageProvider.java
@@ -3,41 +3,50 @@ package com.battlezone.megamachines.storage;
 import com.battlezone.megamachines.math.Vector3f;
 
 /**
- * an interface to define a provider capable for storing and retrieving data in a key-value pair format
+ * an interface to define a provider capable of storing and retrieving data in a key-value pair format
  */
-public interface StorageProvider {
+public abstract class StorageProvider {
 
-    void setValue(String key, String value);
+    public abstract void setValue(String key, String value);
 
-    void setValue(String key, int value);
+    public abstract void setValue(String key, int value);
 
-    void setValue(String key, float value);
+    public abstract void setValue(String key, float value);
 
-    void setValue(String key, double value);
+    public abstract void setValue(String key, double value);
 
-    void setValue(String key, long value);
+    public abstract void setValue(String key, long value);
 
-    void setValue(String key, boolean value);
+    public abstract void setValue(String key, boolean value);
 
-    void setValue(String key, Vector3f value);
+    public void setValue(String key, Vector3f value) {
+        setValue(key + "x", value.x);
+        setValue(key + "y", value.y);
+        setValue(key + "z", value.z);
+    }
 
-    String getString(String key, String def);
+    public abstract String getString(String key, String def);
 
-    int getInt(String key, int def);
+    public abstract int getInt(String key, int def);
 
-    float getFloat(String key, float def);
+    public abstract float getFloat(String key, float def);
 
-    double getDouble(String key, double def);
+    public abstract double getDouble(String key, double def);
 
-    long getLong(String key, long def);
+    public abstract long getLong(String key, long def);
 
-    boolean getBoolean(String key, boolean def);
+    public abstract boolean getBoolean(String key, boolean def);
 
-    Vector3f getVector3f(String key, Vector3f def);
+    public Vector3f getVector3f(String key, Vector3f def) {
+        float x = getFloat(key + "x", def.x);
+        float y = getFloat(key + "y", def.y);
+        float z = getFloat(key + "z", def.z);
+        return new Vector3f(x, y, z);
+    }
 
-    void clearAll();
+    public abstract void clearAll();
 
-    void save();
+    public abstract void save();
 
-    void reload();
+    public abstract void reload();
 }

c4d75e67e721e9dd8fbcc814c9c104cc5bc16d7d
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-02-20 18:36:21 +0000

    collision sound engine

--- src/main/java/com/battlezone/megamachines/sound/SoundFiles.java
+++ src/main/java/com/battlezone/megamachines/sound/SoundFiles.java
@@ -5,6 +5,7 @@ public class SoundFiles {
     public static final String IN_GAME_MUSIC = "/sounds/in_game_music.wav";
     public static final String MENU_MUSIC = "/sounds/menu_music.wav";
     public static final String ENGINE_SOUND = "/sounds/engine_sound.wav";
+    public static final String CRASH_SOUND = "/sounds/crash.wav";
 
 }
 

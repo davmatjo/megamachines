5a91624bb46d3649267b99db9e431f0a93bcbf7b
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-02-22 16:01:43 +0000

    Chunk up the sound menu

--- src/main/java/com/battlezone/megamachines/renderer/ui/SettingsMenuScene.java
+++ src/main/java/com/battlezone/megamachines/renderer/ui/SettingsMenuScene.java
@@ -12,48 +12,79 @@ public class SettingsMenuScene extends MenuScene {
     private AbstractMenu menu;
     private static final int MAX_CAR_MODEL = 3;
 
+    private MenuScene soundSettings;
+    private MenuScene gameSettings;
+
     public SettingsMenuScene(AbstractMenu menu, Vector4f primaryColor, Vector4f secondaryColor, Box background) {
         super(primaryColor, secondaryColor, background);
+
+        this.soundSettings = new MenuScene(Colour.WHITE, Colour.BLUE, background);
+        this.gameSettings = new MenuScene(Colour.WHITE, Colour.BLUE, background);
+
         this.menu = menu;
+        initSound();
+        initGame();
         init();
     }
 
     private void init() {
+
+        addLabel("SETTINGS", 1.5f, 1f, Colour.WHITE);
+        addButton("AUDIO", 0.5f, () -> menu.navigationPush(soundSettings));
+        addButton("GAME", -0.5f, () -> menu.navigationPush(gameSettings));
+        addButton("BACK", -1.5f, () -> {
+            menu.navigationPop();
+        });
+    }
+
+    private void initSound() {
         float backgroundVolume = Storage.getStorage().getFloat(Storage.BACKGROUND_MUSIC_VOLUME, 1);
         float fxVolume = Storage.getStorage().getFloat(Storage.SFX_VOLUME, 1);
+
+        soundSettings.addLabel("SOUND SETTINGS", 1.5f, 0.7f, Colour.WHITE);
+
+        SeekBar backgroundToggle = soundSettings.addSeekbar("BACKGROUND MUSIC", backgroundVolume, 0.5f);
+        backgroundToggle.setOnValueChanged(() -> backgroundVolumeChanged(backgroundToggle));
+
+        SeekBar fxToggle = soundSettings.addSeekbar("SFX", fxVolume, -0.5f);
+        fxToggle.setOnValueChanged(() -> fxVolumeChanged(fxToggle));
+
+        soundSettings.addButton("SAVE AND RETURN", -1.5f, () -> {
+            Storage.getStorage().save();
+            menu.navigationPop();
+        });
+
+        soundSettings.hide();
+    }
+
+    private void initGame() {
         int carModelNumber = Storage.getStorage().getInt(Storage.CAR_MODEL, 1);
         Vector3f carColour = Storage.getStorage().getVector3f(Storage.CAR_COLOUR, new Vector3f(1, 1, 1));
 
-        SeekBar backgroundToggle = addSeekbar("BACKGROUND MUSIC", backgroundVolume, 2);
-        backgroundToggle.setOnValueChanged(() -> backgroundVolumeChanged(backgroundToggle));
+        gameSettings.addLabel("GAME SETTINGS", 1, 0.7f, Colour.WHITE);
 
-        SeekBar fxToggle = addSeekbar("SFX", fxVolume, 1);
-        fxToggle.setOnValueChanged(() -> fxVolumeChanged(fxToggle));
+        Box colourPreview = new Box(BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_X, getButtonY(0), new Vector4f(carColour, 1));
+        gameSettings.addElement(colourPreview);
 
-        Box colourPreview = new Box(BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_X, getButtonY(-1), new Vector4f(carColour, 1));
-        addElement(colourPreview);
+        Box carModel = new Box((BUTTON_WIDTH / 4) - 3 * PADDING, BUTTON_HEIGHT - PADDING, BUTTON_X + getRowX(4, 4), getButtonY(0) + PADDING / 2, new Vector4f(carColour, 1), AssetManager.loadTexture("/cars/car" + carModelNumber + ".png"));
+        gameSettings.addElement(carModel);
 
-        Box carModel = new Box((BUTTON_WIDTH / 4) - 3 * PADDING, BUTTON_HEIGHT - PADDING, BUTTON_X + getRowX(4, 4), getButtonY(-1) + PADDING / 2, new Vector4f(carColour, 1), AssetManager.loadTexture("/cars/car" + carModelNumber + ".png"));
-        addElement(carModel);
-
-        SeekBar carColourX = addSeekbar("R", carColour.x, -1, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(1, 4), PADDING / 2, PADDING * 1.2f);
-        SeekBar carColourY = addSeekbar("G", carColour.y, -1, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(2, 4), PADDING / 2, PADDING * 1.2f);
-        SeekBar carColourZ = addSeekbar("B", carColour.z, -1, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(3, 4), PADDING / 2, PADDING * 1.2f);
+        SeekBar carColourX = gameSettings.addSeekbar("R", carColour.x, 0, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(1, 4), PADDING / 2, PADDING * 1.2f);
+        SeekBar carColourY = gameSettings.addSeekbar("G", carColour.y, 0, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(2, 4), PADDING / 2, PADDING * 1.2f);
+        SeekBar carColourZ = gameSettings.addSeekbar("B", carColour.z, 0, null, BUTTON_WIDTH / 4 - 2 * PADDING, BUTTON_HEIGHT - PADDING, getRowX(3, 4), PADDING / 2, PADDING * 1.2f);
 
         Runnable colourChanged = (() -> carColourChanged(carColourX, carColourY, carColourZ, colourPreview, carModel, carColour));
         carColourX.setOnValueChanged(colourChanged);
         carColourY.setOnValueChanged(colourChanged);
         carColourZ.setOnValueChanged(colourChanged);
 
-        Button toggleCarModel = addButton("CAR MODEL " + carModelNumber, 0);
+        Button toggleCarModel = gameSettings.addButton("CAR MODEL " + carModelNumber, -1);
         toggleCarModel.setAction(() -> carModelChanged(toggleCarModel, carModel));
 
-        addButton("SAVE AND EXIT", -2, () -> {
+        gameSettings.addButton("SAVE AND RETURN", -2, () -> {
             Storage.getStorage().save();
             menu.navigationPop();
         });
-
-        hide();
     }
 
     private void backgroundVolumeChanged(SeekBar seekBar) {

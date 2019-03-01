182ec22e7e820a2e421e981a5d6bcbc02973d220
Kieran Mason <me@kieranmason.co.uk>
2019-03-01 12:56:23 +0000

    Un-deprecated physics test

--- src/test/java/com/battlezone/megamachines/physics/CarBodyTests.java
+++ src/test/java/com/battlezone/megamachines/physics/CarBodyTests.java
@@ -1,14 +1,10 @@
 package com.battlezone.megamachines.physics;
 
 import com.battlezone.megamachines.entities.Cars.AffordThoroughbred;
-import com.battlezone.megamachines.entities.abstractCarComponents.CarBody;
 import com.battlezone.megamachines.math.Vector3f;
-import com.battlezone.megamachines.util.Pair;
 import com.battlezone.megamachines.world.ScaleController;
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
 
 /**
  * The tests for car body
@@ -17,8 +13,8 @@ public class CarBodyTests {
     @Test
     public void correctWeight() {
         PhysicsEngine pe = new PhysicsEngine();
-        AffordThoroughbred at = new AffordThoroughbred(0, 0, ScaleController.RWDCAR_SCALE, 1, new Vector3f(0,0,0), 0, 0);
+        AffordThoroughbred at = new AffordThoroughbred(0, 0, ScaleController.RWDCAR_SCALE, 1, new Vector3f(0, 0, 0), 0, 0);
         pe.addCar(at);
-        assertEquals(800.0, at.getCarBody().getWeight());
+        Assert.assertEquals(800.0, at.getCarBody().getWeight(), 0);
     }
 }

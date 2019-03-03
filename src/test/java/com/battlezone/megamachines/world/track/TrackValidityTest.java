1fe5f7a89b6c690689a101fcb443c70cc5cbac60
Kieran Mason <me@kieranmason.co.uk>
2019-03-03 09:51:55 +0000

    More track validity testing

--- src/test/java/com/battlezone/megamachines/world/track/TrackValidityTest.java
+++ src/test/java/com/battlezone/megamachines/world/track/TrackValidityTest.java
@@ -1,6 +1,7 @@
 package com.battlezone.megamachines.world.track;
 
 import com.battlezone.megamachines.world.track.generator.TrackCircleLoop;
+import com.battlezone.megamachines.world.track.generator.TrackSquareLoop;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -35,8 +36,39 @@ public class TrackValidityTest {
     }
 
     @Test
+    public void noFloatingPiecesTest() {
+        final TrackType[][] grid = {
+                {LEFT_UP, UP, UP, null, LEFT},
+                {LEFT, null, RIGHT, null, null},
+                {DOWN_LEFT, DOWN, RIGHT_DOWN, null, null}
+        };
+        Assert.assertFalse(Track.isValidTrack(grid));
+    }
+
+    @Test
     public void circleValidTest() {
         final Track track = new TrackCircleLoop(10, 10, false).generateTrack();
+        final TrackType[][] grid = track.getGrid();
+        Assert.assertTrue(Track.isValidTrack(grid));
+    }
+
+    @Test
+    public void largeCircleValidTest() {
+        final Track track = new TrackCircleLoop(1000, 1000, false).generateTrack();
+        final TrackType[][] grid = track.getGrid();
+        Assert.assertTrue(Track.isValidTrack(grid));
+    }
+
+    @Test
+    public void squareLoopTest() {
+        final Track track = new TrackSquareLoop(10, 10, false).generateTrack();
+        final TrackType[][] grid = track.getGrid();
+        Assert.assertTrue(Track.isValidTrack(grid));
+    }
+
+    @Test
+    public void largeSquareLoopTest() {
+        final Track track = new TrackSquareLoop(1000, 1000, false).generateTrack();
         final TrackType[][] grid = track.getGrid();
         Assert.assertTrue(Track.isValidTrack(grid));
     }

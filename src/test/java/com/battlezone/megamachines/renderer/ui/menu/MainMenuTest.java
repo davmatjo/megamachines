4413508efed5e0439adf283a8f294f3bb8707d22
Hamzah Malik <hamzahrmalik@yahoo.co.uk>
2019-03-13 19:38:26 +0000

    menu test

---
+++ src/test/java/com/battlezone/megamachines/renderer/ui/menu/MainMenuTest.java
@@ -0,0 +1,30 @@
+package com.battlezone.megamachines.renderer.ui.menu;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+public class MainMenuTest {
+
+    @Test
+    public void push() {
+        MenuScene sceneA = Mockito.mock(MenuScene.class);
+        MenuScene sceneB = Mockito.mock(MenuScene.class);
+
+        AbstractMenu menu = new AbstractMenu();
+
+        menu.navigationPush(sceneA);
+        Assert.assertSame(menu.getCurrentScene(), sceneA);
+
+
+        menu.navigationPush(sceneB);
+
+        Assert.assertSame(menu.getCurrentScene(), sceneB);
+        verify(sceneA, atLeastOnce()).hide();
+
+    }
+
+}

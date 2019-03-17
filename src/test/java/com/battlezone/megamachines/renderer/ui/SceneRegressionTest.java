5d8729c42fabd6e0486902bbbe62ef139ad44128
David Jones <dxj786@student.bham.ac.uk>
2019-03-17 22:02:48 +0000

    Scene error message regression tests

--- src/test/java/com/battlezone/megamachines/renderer/ui/SceneRegressionTest.java
+++ src/test/java/com/battlezone/megamachines/renderer/ui/SceneRegressionTest.java
@@ -1,11 +1,15 @@
 package com.battlezone.megamachines.renderer.ui;
 
+import com.battlezone.megamachines.events.ui.ErrorEvent;
+import com.battlezone.megamachines.messaging.MessageBus;
+import com.battlezone.megamachines.renderer.RenderTestUtil;
 import com.battlezone.megamachines.renderer.Window;
 import com.battlezone.megamachines.renderer.ui.elements.Box;
 import com.battlezone.megamachines.util.AssetManager;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.lwjgl.BufferUtils;
 
 import static org.junit.Assert.*;
 import static org.lwjgl.glfw.GLFW.*;
@@ -43,6 +47,43 @@ public class SceneRegressionTest {
         scene.addElement(box);
 
         scene.render();
+        scene.removeElement(box);
 
+    }
+
+    @Test
+    public void showErrors() {
+        scene.show();
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        MessageBus.fire(new ErrorEvent("TEST", "EVENT", 1));
+        MessageBus.fire(new ErrorEvent("TEST", "EVENT 2", 1, Colour.BLUE));
+
+        scene.render();
+        scene.render();
+        glfwSwapBuffers(windowID);
+        var actual = BufferUtils.createByteBuffer(4 * 1920 * 1080);
+        glReadBuffer(GL_FRONT);
+        glReadPixels(0, 0, 1920, 1080, GL_RGBA, GL_UNSIGNED_BYTE, actual);
+
+        RenderTestUtil.rendersAreEqual("src/test/resources/regression/event1.bmp", actual);
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        for (int i=0; i<63; i++) {
+            glClearColor(0, 0, 0, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            scene.render();
+        }
+        glfwSwapBuffers(windowID);
+        actual = BufferUtils.createByteBuffer(4 * 1920 * 1080);
+        glReadBuffer(GL_FRONT);
+        glReadPixels(0, 0, 1920, 1080, GL_RGBA, GL_UNSIGNED_BYTE, actual);
+
+        RenderTestUtil.rendersAreEqual("src/test/resources/regression/event2.bmp", actual);
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        scene.hide();
     }
 }

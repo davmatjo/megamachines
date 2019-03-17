e8b91a7bdaa7b225eed9f057b70023b9e183c46b
David Jones <dxj786@student.bham.ac.uk>
2019-03-17 17:18:43 +0000

    fix additional tests for headless environment

--- src/test/java/com/battlezone/megamachines/renderer/ui/menu/MainMenuTest.java
+++ src/test/java/com/battlezone/megamachines/renderer/ui/menu/MainMenuTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 
@@ -11,20 +12,21 @@ public class MainMenuTest {
 
     @Test
     public void push() {
-        MenuScene sceneA = Mockito.mock(MenuScene.class);
-        MenuScene sceneB = Mockito.mock(MenuScene.class);
+        if (glfwInit()) {
+            MenuScene sceneA = Mockito.mock(MenuScene.class);
+            MenuScene sceneB = Mockito.mock(MenuScene.class);
 
-        AbstractMenu menu = new AbstractMenu();
+            AbstractMenu menu = new AbstractMenu();
 
-        menu.navigationPush(sceneA);
-        Assert.assertSame(menu.getCurrentScene(), sceneA);
+            menu.navigationPush(sceneA);
+            Assert.assertSame(menu.getCurrentScene(), sceneA);
 
 
-        menu.navigationPush(sceneB);
+            menu.navigationPush(sceneB);
 
-        Assert.assertSame(menu.getCurrentScene(), sceneB);
-        verify(sceneA, atLeastOnce()).hide();
-
+            Assert.assertSame(menu.getCurrentScene(), sceneB);
+            verify(sceneA, atLeastOnce()).hide();
+        }
     }
 
 }

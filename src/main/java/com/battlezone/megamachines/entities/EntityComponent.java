7bdc3454890fd244c33db6929055bf42919d49aa
Stefan Hagiu <sxh1076@cs.bham.ac.uk>
2019-03-19 14:35:42 +0000

    We don't use offsets anymore

--- src/main/java/com/battlezone/megamachines/entities/EntityComponent.java
+++ src/main/java/com/battlezone/megamachines/entities/EntityComponent.java
@@ -6,35 +6,9 @@ package com.battlezone.megamachines.entities;
  */
 public abstract class EntityComponent {
     /**
-     * The offset on the X axis from the original Entity's position
-     */
-    private double XOffset;
-
-    /**
-     * The offset on the Y axis fromthe original Entity's position
-     */
-    private double YOffset;
-
-    /**
      * The weight measured in kilograms
      */
     protected double weight = 0;
-
-    /**
-     * Gets the Entity Component's X Offset
-     * @return The X Offset
-     */
-    public double getXOffset() {
-        return XOffset;
-    }
-
-    /**
-     * Gets the Entity Component's Y Offset
-     * @return The Y offset
-     */
-    public double getYOffset() {
-        return YOffset;
-    }
 
     /**
      * Gets the Entity Component's weight

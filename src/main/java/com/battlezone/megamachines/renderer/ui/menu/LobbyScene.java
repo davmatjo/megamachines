16bb97805505b9fa3f1c598c2d3f98dd89e9d985
David Jones <dxj786@student.bham.ac.uk>
2019-03-21 02:02:22 +0000

    potential fix for 5th player alignment issue

--- src/main/java/com/battlezone/megamachines/renderer/ui/menu/LobbyScene.java
+++ src/main/java/com/battlezone/megamachines/renderer/ui/menu/LobbyScene.java
@@ -57,7 +57,7 @@ public class LobbyScene extends MenuScene {
                             PLAYER_AVATAR_WIDTH,
                             PLAYER_AVATER_HEIGHT,
                             PLAYER_AVATAR_X + (i % (int) Math.ceil((Server.MAX_PLAYERS / 2.0))) * PLAYER_AVATAR_POSITION_OFFSET,
-                            i > Math.ceil(Server.MAX_PLAYERS / 2.0) ? PLAYER_AVATAR_Y_BOTTOM : PLAYER_AVATAR_Y_TOP,
+                            i >= Math.ceil(Server.MAX_PLAYERS / 2.0) ? PLAYER_AVATAR_Y_BOTTOM : PLAYER_AVATAR_Y_TOP,
                             players.get(i).getColour(),
                             AssetManager.loadTexture("/cars/car" + players.get(i).getModelNumber() + ".png")));
         }

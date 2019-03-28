dd5fe983dce7f1d67f8c0cc2d7a9ce7e7ae47d42
Kieran <me@kieranmason.co.uk>
2019-03-28 19:33:37 +0000

    JavaDoc for Math package

--- src/main/java/com/battlezone/megamachines/math/Vector2d.java
+++ src/main/java/com/battlezone/megamachines/math/Vector2d.java
@@ -1,10 +1,19 @@
 package com.battlezone.megamachines.math;
 
+/**
+ * A 2D vector class for doubles.
+ */
 public class Vector2d {
 
     public double x;
     public double y;
 
+    /**
+     * Creates a 2D vector with the set values.
+     *
+     * @param x the X value.
+     * @param y the Y value.
+     */
     public Vector2d(double x, double y) {
         this.x = x;
         this.y = y;
